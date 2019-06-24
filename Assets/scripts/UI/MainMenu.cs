75274b4bf2b7a5ec94b9c0e932f10144ab86505f
Danko37 <via4eslav.bank@yandex.ru>
2019-06-24 18:08:10 +0300

    выход из приложения на esc

---
+++ Assets/scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{   
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
