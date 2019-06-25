c5433661c5aa3213dcfba3249484a438cc4dc166
Danko37 <via4eslav.bank@yandex.ru>
2019-06-25 20:24:34 +0300

    удалил не нужный скрипт Building

--- Assets/scripts/UI/Shop.cs
+++ Assets/scripts/UI/Shop.cs
@@ -14,7 +14,6 @@ public class Shop : MonoBehaviour
 
     public Build_Panel build_Panel;//панелька в интерфейсе где отображается прогресс постройки
 
-    public Building buildingClass;//пока не нужен
 
     public Resourses resourses;
     public ShopInfo ShopInfo;
